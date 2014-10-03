e691fd929359624bf144627d9ef0b4192baccb6d
Vince Duquette <vince.duquette@gmail.com>
2014-10-03 15:28:25 -0400

    mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,12 +83,12 @@ Rails.application.configure do
   
   
   ActionMailer::Base.smtp_settings = {
-      :port =>           '587',
-      :address =>        'smtp.mandrillapp.com',
-      :user_name =>      'vince.duquette@gmail.com',
-      :password =>       'Duqu3tt3!!',
-      :domain =>         'heroku.com',
-      :authentication =>  'login'
+    :port =>           '587',
+    :address =>        'smtp.mandrillapp.com',
+    :user_name =>      ENV['MANDRILL_USERNAME'],
+    :password =>       ENV['MANDRILL_APIKEY'],
+    :domain =>         'heroku.com',
+    :authentication => :plain
   }
   ActionMailer::Base.delivery_method = :smtp
   
